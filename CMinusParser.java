1a917c5a4b9f6af2f5a9c2f7f51c7d0137c33f6b
CalebLBaker <caleblbaker@cedarville.edu>
2018-03-20 21:15:58 -0400

    added signatures for parse methods

--- CMinusParser.java
+++ CMinusParser.java
@@ -1,5 +1,96 @@
 package parser;
 
-public class <class-name> {
+public class CMinusParser {
 
+	private parseProgram() {
+
+	}
+
+	private parseDeclaration() {
+
+	}
+
+	private parseDeclPrime() {
+
+	}
+
+	private parseFunDeclPrime() {
+
+	}
+
+	private parseVarDecl() {
+
+	}
+
+	private parseParams() {
+
+	}
+
+	private parseParam() {
+
+	}
+
+	private parseParamList() {
+
+	}
+
+	private parseCompoundStmt() {
+
+	}
+
+	private parseStatement() {
+
+	}
+
+	private parseExpressionStmt() {
+
+	}
+
+	private parseSelectionStmt() {
+
+	}
+
+	private parseIterationStmt() {
+
+	}
+
+	private parseReturnStmt() {
+
+	}
+
+	private parseExpression() {
+
+	}
+
+	private parseExpressionPrime() {
+
+	}
+
+	private parseExpressionPrimePrime() {
+
+	}
+
+	private parseSimpleExpressionPrime() {
+
+	}
+
+	private parseAdditiveExpression() {
+
+	}
+
+	private parseTerm() {
+
+	}
+
+	private parseFactor() {
+
+	}
+
+	private parseArgs() {
+
+	}
+
+	private parseArgsList() {
+
+	}
 }
