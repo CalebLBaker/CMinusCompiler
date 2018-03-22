58ebad80d366e0b308536a642bafa6d18ed4c461
CalebLBaker <caleblbaker@cedarville.edu>
2018-03-22 16:05:52 -0400

    Implemented ExpressionStatement

--- parser/ExpressionStatement.java
+++ parser/ExpressionStatement.java
@@ -1,5 +1,56 @@
+/**
+* This class represents an expression statement in a C- abstract syntax tree
+*
+* @author Caleb Baker
+* @version 1.0
+* File: ExpressionStatement.java
+* Created: Spring 2018
+* (C)Copyright Cedarville University, its Computer Science faculty, and the
+* authors. All rights reserved.
+*
+* Description: This class represents an expression statement.
+*			   It becomes a node in a C- abstract syntax tree.
+*
+*/
 package parser;
 
-public class ExpressionStatement {
+public class ExpressionStatement extends Statement {
 
+	// Index if variable is an array. Null otherwise.
+	private Expression e;
+
+	/**
+	 * Constructor
+	 * @param exp the expression
+	 */
+	public ExpressionStatement(Expression exp) {
+		e = exp;
+	}
+
+
+	/**
+	 * Null constructor
+	 */
+	public ExpressionStatement() {
+		this(null);
+	}
+
+	/**
+	 * Accessor for the expression
+	 * @return the expression
+	 */
+	public Expression getExpression() {
+		return e;
+	}
+
+	/**
+	 * Prints the node
+	 * @param tab how far to indent the node
+	 */
+	public void print(String tab) {
+		System.out.println(tab + "Expression Statement");
+		if (e != null) {
+			e.print(tab + "    ");
+		}
+	}
 }
