20472058df579a0072464088ab4115fc91bfea3d
CalebLBaker <caleblbaker@cedarville.edu>
2018-03-22 18:05:31 -0400

    Implemented Statement

--- parser/Statement.java
+++ parser/Statement.java
@@ -1,5 +1,26 @@
+/**
+* This class represents an statement in a C- abstract syntax tree
+*
+* @author Caleb Baker
+* @version 1.0
+* File: CallExpression.java
+* Created: Spring 2018
+* (C)Copyright Cedarville University, its Computer Science faculty, and the
+* authors. All rights reserved.
+*
+* Description: This class represents a statement.
+*			   It is an abstract class that more specific expression classes can exted.
+*
+*/
+
+
 package parser;
 
-public class Statement {
+abstract public class Statement {
 
+	/**
+	 * Prints the node
+	 * @param tab how far to indent the node
+	 */
+	abstract public void print(String tab);
 }
