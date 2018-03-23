f043f7deb482854b66e5306d6bca7ade75133ce7
CalebLBaker <caleblbaker@cedarville.edu>
2018-03-22 20:26:41 -0400

    Implemented CompoundStatement

--- parser/VariableDeclaration.java
+++ parser/VariableDeclaration.java
@@ -3,14 +3,18 @@ package parser;
 public class VariableDeclaration extends Declaration {
     private String name;
     private int index;
-    
+
     VariableDeclaration(String n) {
         name = n;
         index = -1;
     }
-    
+
     VariableDeclaration(String n, int i) {
         name = n;
         index = i;
+    }
+
+    public void print(String tab) {
+
     }
 }
