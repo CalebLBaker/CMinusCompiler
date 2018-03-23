748b17833191bba85e23d6aaf2b7285c02956bc3
Faith Trautmann <ftrautmann@cedarville.edu>
2018-03-23 00:19:46 -0400

    comments

--- parser/FunctionDeclaration.java
+++ parser/FunctionDeclaration.java
@@ -4,11 +4,25 @@ import java.util.ArrayList;
 import scanner.Token;
 
 public class FunctionDeclaration extends Declaration {
+    // name of the function 
     private String name;
+    
+    //return type of the function
     private Token.TokenType returnType;
+    
+    // parameters of the function
     private ArrayList<Parameter> parameters;
+    
+    // compound statement the makes up the function body
     private CompoundStatement statement;
 
+    /**
+     * Function declaration constructor
+     * @param t the return type of the function
+     * @param n the name of the array.
+     * @param p the function parameters
+     * @param s the compound statement the makes up the function body
+     */
     FunctionDeclaration( Token.TokenType t, String n, ArrayList<Parameter> p, CompoundStatement s) {
         returnType = t;
         name = n;
--- parser/Parameter.java
+++ parser/Parameter.java
@@ -1,9 +1,17 @@
 package parser;
 
 public class Parameter {
+    // name of the parameter 
     private String name;
+    
+    // true if the parameter is an array, false otherwise
     private boolean isArray;
 
+    /**
+     * Parameter constructor
+     * @param n the name of the parameter.
+     * @param iA is the parameter an array.
+     */
     Parameter(String n, boolean iA) {
         name = n;
         isArray = iA;
