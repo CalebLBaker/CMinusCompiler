e24c9c00adc0be2f03e01ac13c524d802644ab18
CalebLBaker <caleblbaker@cedarville.edu>
2018-03-22 23:30:59 -0400

    fixed but with isTermFollowSet

---
+++ test9.c
@@ -0,0 +1,39 @@
+int add(int a, int b[]) {
+	return a + b[0];
+}
+
+void fun(void) {
+	return;
+}
+
+int fungus[2];
+
+int world;
+
+int main(void) {
+	int x;
+	int taco[12];
+	int y;
+	fun();
+	x = 5 + 17;
+	5 + 2;
+	taco[3] + 2;
+	(x * y) + 13;
+	if (x > 12 * 2) {
+		int z;
+		y = x - 13;
+	}
+	;
+	while (x > y) {
+		y = y + 1;
+		y = add(x, taco) / 2;
+		y + 17 * 13 > 12;
+	}
+	if (y > x) {
+		y = 12;
+	}
+	else {
+		y = 13;
+	}
+	return 0;
+}
