0b3e1c1e1950fcc1ddde19509188323ff1cd8902
CalebLBaker <caleblbaker@cedarville.edu>
2018-03-22 21:36:03 -0400

    Implemented print methods for FunctionDeclaration and Parameter

--- parser/FunctionDeclaration.java
+++ parser/FunctionDeclaration.java
@@ -4,15 +4,29 @@ import java.util.ArrayList;
 import scanner.Token;
 
 public class FunctionDeclaration extends Declaration {
-    private String name;    
+    private String name;
     private Token.TokenType returnType;
     private ArrayList<Parameter> parameters;
     private CompoundStatement statement;
-            
+
     FunctionDeclaration( Token.TokenType t, String n, ArrayList<Parameter> p, CompoundStatement s) {
         returnType = t;
         name = n;
         parameters = p;
-        statement = s;    
+        statement = s;
+    }
+
+    /**
+     * Prints the node
+     * @param tab current level of indentation
+     */
+    public void print(String tab) {
+        System.out.println(tab + Token.toString(returnType) + name + "(");
+        String newTab = tab + "    ";
+        for (Parameter p : parameters) {
+            p.print(newTab);
+        }
+        System.out.println(tab + ")");
+        statement.print(tab);
     }
 }
--- parser/Parameter.java
+++ parser/Parameter.java
@@ -3,9 +3,21 @@ package parser;
 public class Parameter {
     private String name;
     private boolean isArray;
-    
+
     Parameter(String n, boolean iA) {
         name = n;
         isArray = iA;
+    }
+
+    /**
+     * Print this node of the tree
+     * @param tab the current level of indentation
+     */
+    public void print(String tab) {
+    	String n = name;
+    	if (isArray) {
+    		n += "[]";
+    	}
+    	System.out.println(tab + n);
     }
 }
