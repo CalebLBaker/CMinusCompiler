8ca8eeb08f389afacc379d9ec42e1fb263990a1b
Faith Trautmann <ftrautmann@cedarville.edu>
2018-03-23 01:35:07 -0400

    comments

--- parser/Declaration.java
+++ parser/Declaration.java
@@ -1,3 +1,17 @@
+/**
+* Abstract Declaration class
+*
+* @author Caleb Baker, Faith Trautmann
+* @version 1.0
+* File: Declaration.java
+* Created: Spring 2018
+* (C)Copyright Cedarville University, its Computer Science faculty, and the
+* authors. All rights reserved.
+*
+* This is an abstract class that other statement classes inherit from. FunctionDeclaration,
+* and VariableDeclartion both inherit from it. It does not inherit from any classes.
+*
+*/
 package parser;
 
 abstract public class Declaration {
