33daa5239f4274585563c6e1f487e9b8e7beb49c
Faith Trautmann <ftrautmann@cedarville.edu>
2018-04-30 17:42:10 -0400

    made gencode for IterationStatement

--- parser/IterationStatement.java
+++ parser/IterationStatement.java
@@ -13,7 +13,10 @@
 *
 */
 package parser;
+import lowlevel.BasicBlock;
 import lowlevel.Function;
+import lowlevel.Operand;
+import lowlevel.Operation;
 
 public class IterationStatement extends Statement {
 
@@ -60,7 +63,48 @@ public class IterationStatement extends Statement {
 		body.print(tab);
 	}
 
-	public void genCode(Function func, SymbolTable tab) {
-		
+	public void genCode(Function func, SymbolTable tab) throws CodeGenerationException {
+		// Generate code for the branch condition.
+		int condRegNum = condition.genCode(func, tab);
+
+		// Create new basic blocks.
+		BasicBlock prevBlock = func.getCurrBlock();
+		BasicBlock bodyBlock = new BasicBlock(func);
+		BasicBlock postBlock = new BasicBlock(func);
+
+		// Create branch operation.
+		Operation branch = new Operation(Operation.OperationType.BEQ, prevBlock);
+		Operand condReg = new Operand(Operand.OperandType.REGISTER, condRegNum);
+		Operand zilch = new Operand(Operand.OperandType.INTEGER, 0);
+		Operand dest = new Operand(Operand.OperandType.BLOCK, postBlock.getBlockNum());
+                
+		branch.setSrcOperand(0, condReg);
+		branch.setSrcOperand(1, zilch);
+		branch.setSrcOperand(2, dest);
+		prevBlock.appendOper(branch);
+
+		// Append body block to main chain.
+		func.appendToCurrentBlock(bodyBlock);
+
+		// Set current block to be body block
+		func.setCurrBlock(bodyBlock);
+
+		// Generate code for the body
+		body.genCode(func, tab);
+                
+                // Create branch operation.
+		Operation iterBranch = new Operation(Operation.OperationType.BNE, bodyBlock);
+                Operand iterDest = new Operand(Operand.OperandType.BLOCK, bodyBlock.getBlockNum());
+
+		branch.setSrcOperand(0, condReg);
+		branch.setSrcOperand(1, zilch);
+		branch.setSrcOperand(2, iterDest);
+		prevBlock.appendOper(iterBranch);
+                
+		// Append post block to main chain.
+		func.appendToCurrentBlock(postBlock);
+                
+		// Set current block to be the post block.
+		func.setCurrBlock(postBlock);
 	}
 }
